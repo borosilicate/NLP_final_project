fbdd1a75799ddacd9bbaa03c4bba6b052e964fc6
michael hearn <mjh55154@uga.edu>
2019-12-01 12:48:51 -0500

    updated with more analysis resources

--- nlp.py
+++ nlp.py
@@ -4,11 +4,57 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
+
 nlp = spacy.load("en_core_web_lg")
 #model=gensim.models.Word2Vec('The monkey runs fast. The eagle is a horse.')
 
 aar=np.load('data/agency_rank_ar.npy')
 car=np.load('data/communion_rank_ar.npy')
+score_dic={}
+words=''
+for k in aar[:]:
+    words+=str(k[1])+' '
+    score_dic[str(k[1])]=k[0]
+for k in car[:]:
+    words+=str(k[1])+' '
+    score_dic[str(k[1])]=k[0]
+model=nlp(words)
+z=np.zeros([len(model)+1,len(model)+1])
+harvest=z
+for i,t1 in enumerate(model):
+    for j,t2 in enumerate(model):
+        z[i,j]+=t1.similarity(t2)
+    print('still running',len(model)-i)
+
+fig, ax = plt.subplots()
+im = ax.imshow(harvest)
+
+# We want to show all ticks...
+print('starting axis')
+ax.set_xticks(np.arange(len(model)))
+ax.set_yticks(np.arange(len(model)))
+# ... and label them with the respective list entries
+ax.set_xticklabels(words.split())
+ax.set_yticklabels(words.split())
+print('done with axis')
+# Rotate the tick labels and set their alignment.
+#plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+#         rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+#for i in range(len(model)):
+#    for j in range(len(model)):
+#        text = ax.text(j, i, harvest[i, j],
+#                       ha="center", va="center", color="w")
+#        print('setting text',len(model)-i)
+
+ax.set_title("Agency vs Communion Similarity")
+fig.tight_layout()
+print('plt.show')
+plt.show()
+#plt.matshow(z)
+#plt.show()
 
 '''
 f=open('pulled/Letters_to_Madame_Hanska/Letters_during_1833.txt')
